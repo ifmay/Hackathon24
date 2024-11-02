7968c2b056a42f8be668dc1e4905d1531e0e8ee9
Isabelle May <imay2@zagmail.gonzaga.edu>
2024-11-02 13:23:46 -0700

    centered map on spo, changed colors!

--- zag-watch/src/main.js
+++ zag-watch/src/main.js
@@ -5,21 +5,23 @@ const containerStyle = {
   height: "400px",
 };
 
+// Coordinates for Spokane
 const center = {
-  lat: 47.6062, // change to spo
-  lng: -122.3321,
+  lat: 47.6586, // Spokane latitude
+  lng: -117.4260, // Spokane longitude
 };
 
 function MainPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Google Maps Integration</h1>
+        <h1>Zag Watch</h1>
+        <h2>Scare Aware</h2>
         <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
           <GoogleMap
             mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
+            center={center} // Center the map on Spokane
+            zoom={10} // Zoom level for visibility
           >
             {/* Child components, like markers, info windows, etc. */}
           </GoogleMap>

acd0439a0b545df05403e7bc5aae5b21a8e27f11
Isabelle May <imay2@zagmail.gonzaga.edu>
2024-11-02 16:22:09 -0700

    Update main.js

--- zag-watch/src/main.js
+++ zag-watch/src/main.js
@@ -25,7 +25,7 @@ function MainPage() {
         initMap();
       } else {
         const script = document.createElement("script");
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAn2L7XjusuIyjQ19kmmpsdlytyKOBvIr0&libraries=places`;
         script.async = true;
         script.defer = true;
         script.onload = initMap;
